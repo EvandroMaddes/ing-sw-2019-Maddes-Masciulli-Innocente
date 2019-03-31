a9ae94dbe34eca3135290e796c03d1258ae4feef
EvandroMaddes <Emanuele2001evandro>
2019-03-31 13:02:23 +0100

    Aggiunta classi logica di gioco e azioni.

---
+++ src/main/java/it/polimi/ingsw/model/Round.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.model;
+
+public class Round {
+    private Player currentPlayer;
+    private Action currentaction;
+
+    public void selectAction(){
+
+    }
+
+    public void managePoints(){
+
+    }
+
+    public void manageKills(){
+
+    }
+
+    public void respawn(){
+
+    }
+}
