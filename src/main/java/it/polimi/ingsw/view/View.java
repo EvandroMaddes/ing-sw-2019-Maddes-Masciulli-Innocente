16dfa04326f323a6cb3d27263c877a99b646be4e
InnocenteFederico <federico.innocente@mail.polimi.it>
2019-04-06 17:42:42 +0200

    Titolo: Classe View e package view
    Aggiunto il package view e aggiunta al suo interno la classe View con lo scheletro dei suoi metodi

---
+++ src/main/java/it/polimi/ingsw/view/View.java
@@ -0,0 +1,57 @@
+package it.polimi.ingsw.view;
+
+import java.util.Observer;
+import java.util.Observable;
+
+public class View extends Observable implements Observer {
+
+    private GameModel gameModel;
+
+
+    public void getPlayer()
+    {
+
+    }
+
+    public void setPlayer()
+    {
+
+    }
+
+    public void getPlayerChoice()
+    {
+
+    }
+
+    public void setMap()
+    {
+
+    }
+
+    public void setMode()
+    {
+
+    }
+
+    public void setActionChoice()
+    {
+
+    }
+
+    public void showResult()
+    {
+
+    }
+
+    @Override
+    public void update(Observable observable, Object arg )
+    {
+
+    }
+
+    @Override
+    public void notifyObservers() {
+        super.notifyObservers();
+    }
+
+}
