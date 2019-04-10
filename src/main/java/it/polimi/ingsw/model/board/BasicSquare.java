b3947728c1f8512cc875ad02d23e7449392d7515
EvandroMaddes <Emanuele2001evandro>
2019-04-10 17:03:52 +0100

    Sviluppo codice metodi package board: sviluppati metodi classi BasicSquare,Direction,DominationSpawnSquare, DominationTrack,Room,SpawnSuare,square.

--- src/main/java/it/polimi/ingsw/model/board/BasicSquare.java
+++ src/main/java/it/polimi/ingsw/model/board/BasicSquare.java
@@ -1,7 +1,14 @@
 package it.polimi.ingsw.model.board;
 
 import it.polimi.ingsw.model.game_components.ammo.AmmoTile;
+import it.polimi.ingsw.model.game_components.ammo.CubeAmmoTile;
+import it.polimi.ingsw.model.game_components.cards.AmmoTilesDeck;
+import it.polimi.ingsw.model.player.Player;
 
+/**
+ * @author Evandro Maddes
+ * Class represents tha square tnat contains an ammo card
+ */
 public class BasicSquare extends Square {
 
     private AmmoTile ammo;
@@ -12,18 +19,36 @@ public class BasicSquare extends Square {
      */
     public boolean checkAmmo(){
         boolean i=true;
+        if(ammo==null)
+        {
+            i=false;
+        }
+        else i=true;
         return i;
 
     }
 
     /**
-     * it calls metod
+     * it calls metod of AmmoTile to grab Ammo
+     * @param ammo this is picked-up by a player
+     * @param player who receives the ammo
      */
-    public void grabAmmoTile(){
+    public void grabAmmoTile(AmmoTile ammo, Player player){
+
+           ammo.pickAmmo(player);
 
     }
 
-    public void replaceAmmoTile(){
+    /**
+     * if one square(excepted spawn square) do not have a ammo,it adds one.
+     *
+     * @param ammoTileCard card taht is drawed
+     */
+    public void replaceAmmoTile(AmmoTile ammoTileCard){
+        if(checkAmmo()==false)
+        {
+           ammo = ammoTileCard;
+        }
 
     }
 }
