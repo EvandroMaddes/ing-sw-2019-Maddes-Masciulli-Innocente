d21c6220afc5e5d3a1010c8261070ace7b3fe78b
InnocenteFederico <federico.innocente@mail.polimi.it>
2019-04-14 19:08:32 +0200

    Update classi Player e PlayerBoard:
    Classe player:
    Implementati i seguenti metodi:
    - Costruttore
    - addAmmo(AmmoCube)
    - addPowerUp(PowerUp)
    - getCubeColourNumber(CubeColour)
    - metodi getter vari
    Aggiunti commenti con standard JUnit
    
    Classe PlayerBoard:
    Implementati i seguenti metodi:
    - addDamages(Player, int)
    - resetDamages
    - inflictMarks(Player)
    - addMarks(Player, int)
    - checkNumberOfMarks(Player)
    - checkAdrenalinicGrab
    - checkAdrenalinicShot
    - setFinalFrenzyScoreBoard
    - metodi getter vari
    Aggiunti commenti con standard JUnit
    Aggiunte le costanti MAX_DAMAGE e MAX_MARKS
    
    Entrambe le classi implementano ora l'interfaccia Iterator, che però progettiamo di reimplementare. Ad ogni modo, i metodi della suddetta interfaccia non sono ancora stati definiti.

--- src/main/java/it/polimi/ingsw/model/player/PlayerBoard.java
+++ src/main/java/it/polimi/ingsw/model/player/PlayerBoard.java
@@ -1,65 +1,165 @@
 package it.polimi.ingsw.model.player;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class PlayerBoard {
+/**
+ * Class to manage the player board
+ *
+ * @author Federico Innocente
+ *
+ */
+public class PlayerBoard implements Iterator {
+
+    private static final int MAX_DAMAGE = 12;
+    private static final int MAX_MARKS = 3;
+    private static final int points[] = new int[]{ 8,6,4,2,1,1 };
 
     private DamageToken[] damageReceved;
+    private int damageAmount;
     private int skullsNumber;
     private ArrayList<DamageToken> marks;
-    final static int points[] = new int[]{ 8,6,4,2,1,1 };
 
-    public void addDamages( Player player, int numberOfDamage )
-    {
 
-    }
-
-    public void addMarks( Player player, int numberOfMarks )
-    {
-
-    }
-
+    /**
+     *
+     * @return skullsNumber
+     */
     public int getSkullsNumber()
     {
         return skullsNumber;
     }
 
-    public void resetDamages()
-    {
-
-    }
-
+    /**
+     * Give a skull to a player
+     */
     public void addSkull()
     {
         skullsNumber += 1;
     }
 
+    /**
+     *
+     * @param player is the the enemy who damage the player
+     * @param numberOfDamage is the number of damage tokens
+     *
+     *
+     */
+    public void addDamages( Player player, int numberOfDamage )
+    {
+        while (damageAmount < MAX_DAMAGE && numberOfDamage > 0)
+        {
+            damageAmount++;
+            damageReceved[damageAmount] = new DamageToken(player);
+            numberOfDamage--;
+        }
+
+        inflictMarks(player);
+    }
+
+    /**
+     * Clear the damage receved by a player, setting them to 0
+     */
+    public void resetDamages()
+    {
+        damageAmount = 0;
+    }
+
+    /**
+     * Remove the marks from the list and add them into the damage receved
+     *
+     * @param player is the player which marks we have to add
+     *
+     */
+    public void inflictMarks(Player player)
+    {
+        Iterator iterator = marks.iterator();
+        DamageToken mark;
+
+        while (iterator.hasNext())
+        {
+            mark = (DamageToken)iterator.next();
+
+            if (mark.getCharacter() == player.getCharacter())
+            {
+                if (damageAmount < 12)
+                {
+                    damageAmount++;
+                    damageReceved[damageAmount] = mark;
+                }
+                iterator.remove();
+            }
+        }
+    }
+
+    /**
+     * Add the marks given by player. The marks that a player has can't be more than MAX_MARKS (=3)
+     *
+     * @param player is the player who place the marks
+     * @param numberOfMarks is the number of marks given
+     */
+    public void addMarks( Player player, int numberOfMarks )
+    {
+        int previousMarsk = checkNumberOfMarks(player);
+
+        if (previousMarsk + numberOfMarks > MAX_MARKS)
+        {
+            numberOfMarks = MAX_MARKS - previousMarsk;
+        }
+        for (int i = 0; i < numberOfMarks; i++)
+        {
+            this.marks.add(new DamageToken(player));
+        }
+    }
+
+    /**
+     * count the number of marks inflicted by a player
+     *
+     * @param player is the player of whom we want to count the marks
+     * @return the number of marks inflict by player
+     */
+    public int checkNumberOfMarks(Player player)
+    {
+        Iterator  iterator = marks.iterator();
+        DamageToken mark;
+        int numberOfMarks = 0;
+
+        while (iterator.hasNext())
+        {
+            mark = (DamageToken) iterator.next();
+            if (mark.getCharacter() == player.getCharacter())
+                numberOfMarks++;
+        }
+
+        return numberOfMarks;
+    }
+
+    /**
+     *
+     * @return is true if damageAmount is greater than 2
+     */
     public boolean checkAdrenalinicGrab()
     {
-        boolean i =true;
-        return i;
+        return damageAmount > 2;
     }
 
+    /**
+     *
+     * @return true if damageAmount is greater than 5
+     */
     public boolean checkAdrenalinicShot()
     {
-        boolean i =true;
-        return i;
+        return damageAmount > 5;
     }
 
-    public void  setFinalFrenzyBoard()
+    /**
+     * In the final frenzy phase, if the player has 0 damage, set the score board of the player at 2-1-1...
+     */
+    public void  setFinalFrenzyScoreBoard()
     {
-
+        if(damageAmount == 0)
+        {
+            skullsNumber = 3;
+        }
     }
-
-    // contiene un return di default per poter eseguire sonar
-    public int checkNumberOfMarks(Player enemy)
-    {
-        return 1;
-    }
-
-    public void removeMarks (Player enemy)
-    {
-
-    }
-
 }
