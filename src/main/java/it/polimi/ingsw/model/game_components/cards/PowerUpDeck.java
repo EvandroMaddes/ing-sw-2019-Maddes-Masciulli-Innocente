080230fe1528a085fdd2f181b65def6397296cc5
EvandroMaddes <Emanuele2001evandro>
2019-05-08 16:11:56 +0200

    -Aggiunto metodo per la creazione del deck di Power Up

--- src/main/java/it/polimi/ingsw/model/game_components/cards/PowerUpDeck.java
+++ src/main/java/it/polimi/ingsw/model/game_components/cards/PowerUpDeck.java
@@ -1,6 +1,10 @@
 package it.polimi.ingsw.model.game_components.cards;
 
+import it.polimi.ingsw.model.game_components.ammo.CubeColour;
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * @author Francesco Masciulli
@@ -40,5 +44,46 @@ public class PowerUpDeck extends DeckManagement {
         setDeck((ArrayList<Object>)discardDeck.clone());
         shuffle();
         discardDeck.clear();
+    }
+
+    /**
+     * @author Evandro Maddes
+     * It creates the powerUp deck and it shuffles it
+     * @return power Up deck
+     */
+    public ArrayList<Object> createPowerupDeck(){
+
+        ArrayList<Object> powerUpDeck = new ArrayList<Object>();
+
+        addPowerupColour(powerUpDeck,CubeColour.Blue);
+        addPowerupColour(powerUpDeck,CubeColour.Yellow);
+        addPowerupColour(powerUpDeck,CubeColour.Red);
+
+
+         Collections.shuffle(powerUpDeck);
+         return powerUpDeck;
+
+    }
+
+    /**
+     * @author Evandro Maddes
+     * While power up deck is partially created, this method addss powers Up by colour
+     * @param currentDeck Power Up deck partially created
+     * @param colour colour of power up
+     * @return power up deck within power up of spicified colour
+     */
+    public ArrayList<Object> addPowerupColour(ArrayList<Object> currentDeck, CubeColour colour){
+
+        for(int i=0; i<2; i++){
+            PowerUp currentTagbackGranade = new TagbackGrenade(colour);
+            currentDeck.add(currentTagbackGranade);
+            PowerUp currentTargetingScope = new TargetingScope(colour);
+            currentDeck.add(currentTargetingScope);
+            PowerUp currentTeleporter = new Teleporter(colour);
+            currentDeck.add(currentTeleporter);
+            PowerUp currentNewton = new Newton(colour);
+            currentDeck.add(currentNewton);
+        }
+        return currentDeck;
     }
 }
