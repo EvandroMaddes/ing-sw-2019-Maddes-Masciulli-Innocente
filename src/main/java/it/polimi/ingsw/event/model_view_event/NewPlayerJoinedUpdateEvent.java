1569a0cad507926ad2deb66042e83e38a06b0ffd
evandro <evandro.maddes@mail.polimi.it>
2019-05-26 14:04:42 +0200

    Aggiunto metodo per la gestione dell'UpdateEvent nuovo giocatore aggiunto alla partita

--- src/main/java/it/polimi/ingsw/event/model_view_event/NewPlayerJoinedUpdateEvent.java
+++ src/main/java/it/polimi/ingsw/event/model_view_event/NewPlayerJoinedUpdateEvent.java
@@ -10,15 +10,25 @@ import it.polimi.ingsw.view.RemoteView;
  */
 public class NewPlayerJoinedUpdateEvent extends ModelViewEvent {
 
+    String newPlayer;
+
+    public String getNewPlayer() {
+        return newPlayer;
+    }
+
+    public void setNewPlayer(String newPlayer) {
+        this.newPlayer = newPlayer;
+    }
 
     public NewPlayerJoinedUpdateEvent(String user){
         super(user);
     }
 
+
     @Override
     public Event performAction(RemoteView remoteView) {
         //todo aggiorna la risorsa sul client
 
-        return null;
+        return remoteView.newPlayerJoinedUpdate(getNewPlayer());
     }
 }
