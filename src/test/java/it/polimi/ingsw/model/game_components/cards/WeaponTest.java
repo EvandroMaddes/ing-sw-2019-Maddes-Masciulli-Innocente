b491354c82ce13f9b5a8335d54c45219dad9968a
frank <franci01@hotmail.it>
2019-06-05 15:48:39 +0200

    Update WeaponTest: armi testate = 6 ritoccate classi durante il testing;

--- src/test/java/it/polimi/ingsw/model/game_components/cards/WeaponTest.java
+++ src/test/java/it/polimi/ingsw/model/game_components/cards/WeaponTest.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.model.game_components.cards;
 
 import it.polimi.ingsw.event.Event;
 import it.polimi.ingsw.event.controller_view_event.TargetPlayerRequestEvent;
+import it.polimi.ingsw.event.controller_view_event.TargetSquareRequestEvent;
 import it.polimi.ingsw.model.board.BasicSquare;
 import it.polimi.ingsw.model.board.Map;
 import it.polimi.ingsw.model.board.Square;
@@ -20,6 +21,12 @@ import java.util.ArrayList;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
+/**
+ * @author Francesco Masciulli
+ * This is the Weapons TestClass:
+ *  every test method check, for each weapon, the correct targets encoded in the Event
+ *  and the execution of every effect.
+ */
 public class WeaponTest {
     private WeaponDeck weaponDeck;
     private Weapon currentWeapon;
@@ -63,7 +70,9 @@ public class WeaponTest {
     }
 
 
-    //public OneOptionalEffectWeapon(CubeColour colour, String name, AmmoCube[] reloadCost, AmmoCube[] secondEffectCost)
+    /**
+     * Test for the LockRifle Class
+     */
     @Test
     public void testLockRifle(){
 
@@ -71,12 +80,12 @@ public class WeaponTest {
         currentWeapon = new LockRifle(CubeColour.Blue,"LockRifle",currentReloadCost,new AmmoCube[] {new AmmoCube(CubeColour.Red)});
         currentWeapon.setOwner(player2);
         Assert.assertTrue(currentWeapon.isUsable());
-        Event targetEvent = currentWeapon.getTargetEffect(1);
         Assert.assertTrue(currentWeapon.isUsableEffect(1));
         Assert.assertFalse(currentWeapon.isUsableEffect(2));
 
 
         //Testing first effect:
+        Event targetEvent = currentWeapon.getTargetEffect(1);
         currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
         Assert.assertTrue(currentTargets.contains(player1.getCharacter()));
         Assert.assertTrue(currentTargets.contains(player3.getCharacter()));
@@ -97,14 +106,16 @@ public class WeaponTest {
         currentWeapon.performEffect(2, testTargets);
         Assert.assertEquals(1,playerBoard3.checkNumberOfMarks(player2));
 
-        System.out.println("Tested LockRifle! ︻┳═一- - - ");
+        System.out.println("Tested LockRifle!\t ︻┳═一- - - \n");
     }
 
-
+    /**
+     * Test for the Electroschyte Class
+     */
     @Test
     public void testElectroschyte(){
         currentReloadCost = new AmmoCube[] { new AmmoCube(CubeColour.Blue)};
-        currentWeapon = new Electroscythe(CubeColour.Blue,"LockRifle",currentReloadCost,new AmmoCube[] {new AmmoCube(CubeColour.Blue), new AmmoCube(CubeColour.Red)});
+        currentWeapon = new Electroscythe(CubeColour.Blue,"Electroschyte",currentReloadCost,new AmmoCube[] {new AmmoCube(CubeColour.Blue), new AmmoCube(CubeColour.Red)});
         currentWeapon.setOwner(player2);
         Assert.assertTrue(currentWeapon.isUsable());
         //This is a melee weapon, so all the player must be on the same square
@@ -142,21 +153,24 @@ public class WeaponTest {
         totalToken = playerBoard1.getDamageAmount()+playerBoard3.getDamageAmount();
         Assert.assertEquals(4, totalToken);
 
-        System.out.println("Tested Electroschyte! ︻┳═一- - - ");
+        System.out.println("Tested Electroschyte!\t ︻┳═一- - - \n");
     }
 
+    /**
+     * Test for the MachineGun Class
+     */
     @Test
     public void testMachineGun(){
         currentReloadCost = new AmmoCube[] { new AmmoCube(CubeColour.Blue), new AmmoCube(CubeColour.Red)};
-        currentWeapon = new Electroscythe(CubeColour.Blue,"LockRifle",currentReloadCost,
-                        new AmmoCube[] {new AmmoCube(CubeColour.Yellow), new AmmoCube(CubeColour.Blue)});
+        currentWeapon = new MachineGun(CubeColour.Blue,"MachineGun",currentReloadCost,
+                        new AmmoCube[] {new AmmoCube(CubeColour.Yellow)},new AmmoCube[] {new AmmoCube(CubeColour.Blue)});
         currentWeapon.setOwner(player2);
         Assert.assertTrue(currentWeapon.isUsable());
         Player tempPlayer = new Player("User", Character.VIOLET);
         tempPlayer.setPosition(player1.getPosition());
-//        Assert.assertTrue(currentWeapon.isUsableEffect(1));
-//        Assert.assertTrue(currentWeapon.isUsableEffect(2));
-//        Assert.assertTrue(currentWeapon.isUsableEffect(3));
+        Assert.assertTrue(currentWeapon.isUsableEffect(1));
+        Assert.assertFalse(currentWeapon.isUsableEffect(2));
+        Assert.assertFalse(currentWeapon.isUsableEffect(3));
 
         //Testing first effect:
         Event targetEvent = currentWeapon.getTargetEffect(1);
@@ -168,11 +182,188 @@ public class WeaponTest {
         testTargets.add(player1);
         testTargets.add(player3);
         testTargets.add(tempPlayer);
-        currentWeapon.performEffect(3, testTargets);
+        currentWeapon.performEffect(1, testTargets.subList(0,2));
         Assert.assertEquals(player2, playerBoard1.getDamageReceived()[0].getPlayer());
+        Assert.assertEquals(player2, playerBoard3.getDamageReceived()[0].getPlayer());
+        Assert.assertEquals(1, playerBoard3.getDamageAmount());
 
+        currentWeapon.performEffect(2,testTargets.subList(0,1));
+        Assert.assertEquals(player2, playerBoard1.getDamageReceived()[1].getPlayer());
+
+    
+        currentWeapon.performEffect(3, testTargets.subList(2,3));
+        Assert.assertEquals(player2, tempPlayer.getPlayerBoard().getDamageReceived()[0].getPlayer());
+        Assert.assertEquals(player2, playerBoard3.getDamageReceived()[1].getPlayer());
+
+        System.out.println("Tested MachineGun! \t︻┳═一- - - \n");
+    }
+
+    /**
+     * Test for the TractorBeam Class
+     */
+    @Test
+    public void testTractorBeam(){
+        currentReloadCost = new AmmoCube[] { new AmmoCube(CubeColour.Blue)};
+        currentWeapon = new TractorBeam(CubeColour.Blue,"TractorBeam",currentReloadCost,
+                new AmmoCube[] {new AmmoCube(CubeColour.Red), new AmmoCube(CubeColour.Yellow)});
+        currentWeapon.setOwner(player2);
+        //trying to move a target that isn't visible at the beginning, according with the Weapon Guide
+        player1.setPosition(testedMapMatrix[1][0]);
+        Assert.assertTrue(currentWeapon.isUsable());
+        Assert.assertTrue(currentWeapon.isUsableEffect(1));
+        Assert.assertFalse(currentWeapon.isUsableEffect(2));
+
+        //Testing first effect:
+        Event targetEvent = currentWeapon.getTargetEffect(1);
+        currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
+
+        Assert.assertTrue(currentTargets.contains(player1.getCharacter()));
+        Assert.assertTrue(currentTargets.contains(player3.getCharacter()));
+
+        testTargets.add(player1);
+        testTargets.add(testedMapMatrix[0][1]);
+        currentWeapon.performEffect(1, testTargets.subList(0,1));
+        currentWeapon.performEffect(1, testTargets.subList(1,2));
+        Assert.assertEquals(player2, playerBoard1.getDamageReceived()[0].getPlayer());
+        Assert.assertEquals(testedMapMatrix[0][1], player1.getPosition());
+        Assert.assertEquals(1, playerBoard1.getDamageAmount());
+
+        //Testing alternative effect:
+        player1.getPosition().removeCurrentPlayer(player1);
+        player1.setPosition(testedMapMatrix[1][0]);
+
+        testTargets.clear();
+        targetEvent = currentWeapon.getTargetEffect(2);
+        currentWeapon.setLoaded();
+        currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
+        Assert.assertFalse(currentTargets.contains(player1.getCharacter()));
+        Assert.assertTrue(currentTargets.contains(player3.getCharacter()));
+        testTargets.add(player3);
+
+        currentWeapon.performEffect(2, testTargets.subList(0,1));
+        Assert.assertEquals(player2, playerBoard3.getDamageReceived()[0].getPlayer());
+        Assert.assertEquals(3,playerBoard3.getDamageAmount());
+
+        System.out.println("Tested TractorBeam! \t︻┳═一- - - \n");
+    }
+
+    /**
+     * Test for the Thor Class
+     */
+    @Test
+    public void testTHOR(){
+        currentReloadCost = new AmmoCube[] { new AmmoCube(CubeColour.Blue), new AmmoCube(CubeColour.Red)};
+        currentWeapon = new Thor(CubeColour.Blue,"Thor",currentReloadCost,
+                new AmmoCube[] {new AmmoCube(CubeColour.Blue)}, new AmmoCube[] {new AmmoCube(CubeColour.Blue)});
+        currentWeapon.setOwner(player1);
+        //Creating another player that is seen just by player 3
+        Player tempPlayer = new Player("User", Character.SPROG);
+        tempPlayer.setPosition(testedMapMatrix[0][3]);
+
+        Assert.assertTrue(currentWeapon.isUsableEffect(1));
+        Assert.assertFalse(currentWeapon.isUsableEffect(2));
+        Assert.assertFalse(currentWeapon.isUsableEffect(3));
+
+        //Testing first effect:
+        Event targetEvent = currentWeapon.getTargetEffect(1);
+        currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
+
+        Assert.assertTrue(currentTargets.contains(player2.getCharacter()));
+        Assert.assertFalse(currentTargets.contains(player3.getCharacter()));
+        Assert.assertFalse(currentTargets.contains(tempPlayer.getCharacter()));
+
+        testTargets.add(player2);
+        currentWeapon.performEffect(1, testTargets);
+        Assert.assertEquals(player1, playerBoard2.getDamageReceived()[1].getPlayer());
+        Assert.assertEquals(2, playerBoard2.getDamageAmount());
+
+        //Testing second (additional) effect:
+        targetEvent = currentWeapon.getTargetEffect(2);
+        currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
+
+        Assert.assertTrue(currentTargets.contains(player3.getCharacter()));
+        Assert.assertFalse(currentTargets.contains(player2.getCharacter()));
+        Assert.assertFalse(currentTargets.contains(tempPlayer.getCharacter()));
+
+        testTargets.clear();
+        testTargets.add(player3);
+        currentWeapon.performEffect(2, testTargets);
+        Assert.assertEquals(player1, playerBoard3.getDamageReceived()[0].getPlayer());
+        Assert.assertEquals(1, playerBoard3.getDamageAmount());
+
+        //Testing third (additional) effect:
+        targetEvent = currentWeapon.getTargetEffect(3);
+        currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
+
+        Assert.assertTrue(currentTargets.contains(tempPlayer.getCharacter()));
+        Assert.assertFalse(currentTargets.contains(player2.getCharacter()));
+        Assert.assertFalse(currentTargets.contains(player3.getCharacter()));
+
+        testTargets.clear();
+        testTargets.add(tempPlayer);
+        currentWeapon.performEffect(3, testTargets);
+        Assert.assertEquals(player1, tempPlayer.getPlayerBoard().getDamageReceived()[1].getPlayer());
+        Assert.assertEquals(2, tempPlayer.getPlayerBoard().getDamageAmount());
+
+        System.out.println("Tested THOR! \t︻┳═一- - - \n");
+    }
+
+    @Test
+    //todo da implementare
+    public void testVortexCannon(){
 
     }
+
+    /**
+     * Test for the PlasmaGun Class
+     */
+    @Test
+    public void testPlasmaGun(){
+        currentReloadCost = new AmmoCube[] { new AmmoCube(CubeColour.Blue), new AmmoCube(CubeColour.Yellow)};
+        currentWeapon = new PlasmaGun(CubeColour.Blue,"PlasmaGun",currentReloadCost,
+                    new AmmoCube[] {}, new AmmoCube[] {new AmmoCube(CubeColour.Blue)});
+        currentWeapon.setOwner(player2);
+        Assert.assertTrue(currentWeapon.isUsable());
+        Assert.assertTrue(currentWeapon.isUsableEffect(1));
+        Assert.assertFalse(currentWeapon.isUsableEffect(2));
+        Assert.assertFalse(currentWeapon.isUsableEffect(3));
+
+        //testing first effect:
+        Event targetEvent = currentWeapon.getTargetEffect(1);
+        currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
+        Assert.assertTrue(currentTargets.contains(player1.getCharacter()));
+        Assert.assertTrue(currentTargets.contains(player3.getCharacter()));
+
+        testTargets.add(player1);
+        currentWeapon.performEffect(1, testTargets);
+        Assert.assertEquals(player2, playerBoard1.getDamageReceived()[1].getPlayer());
+        Assert.assertEquals(2, playerBoard1.getDamageAmount());
+
+        //Testing second(additional) effect:
+        //NB non contiene lo square dove è attualmente il proprietario dell'arma
+        targetEvent = currentWeapon.getTargetEffect(2);
+        int totalAvailableSquares = ((TargetSquareRequestEvent)targetEvent).getPossibleTargetsX().length;
+        Assert.assertEquals(6, totalAvailableSquares);
+        testTargets.clear();
+        testTargets.add(square3);
+        currentWeapon.performEffect(2, testTargets);
+        Assert.assertEquals(square3, player2.getPosition());
+
+        //testing third(additional) effect:
+        targetEvent = currentWeapon.getTargetEffect(3);
+        currentTargets = ((TargetPlayerRequestEvent)targetEvent).getPossibleTargets();
+        Assert.assertTrue(currentTargets.contains(player1.getCharacter()));
+        Assert.assertFalse(currentTargets.contains(player3.getCharacter()));
+
+        //this additional effect doesn't need any target, it deal an additional damage to the first target
+        currentWeapon.performEffect(3, null);
+        Assert.assertEquals(player2, playerBoard1.getDamageReceived()[2].getPlayer());
+        Assert.assertEquals(3, playerBoard1.getDamageAmount());
+
+        System.out.println("Tested PlasmaGun!\t ︻┳═一- - - \n");
+    }
+
+
 
 
     /**
