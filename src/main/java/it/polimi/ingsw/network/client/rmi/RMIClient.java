1d19a82b34835b18aed5e6acdcf06777c783654c
evandro <evandro.maddes@mail.polimi.it>
2019-06-06 12:08:59 +0200

    cambiata porta rmiClient

--- src/main/java/it/polimi/ingsw/network/client/rmi/RMIClient.java
+++ src/main/java/it/polimi/ingsw/network/client/rmi/RMIClient.java
@@ -106,7 +106,7 @@ public class RMIClient extends UnicastRemoteObject implements ClientInterface, N
             try {
                 UnicastRemoteObject.unexportObject(this,false);
                 int clientNumber = server.getClientListNumber()+1;
-                RemoteInterface clientStub = (RemoteInterface) UnicastRemoteObject.exportObject(this,clientNumber);
+                RemoteInterface clientStub = (RemoteInterface) UnicastRemoteObject.exportObject(this,0);
                 Registry clientRegistry = LocateRegistry.createRegistry(port);
                 clientRegistry.rebind("RMIClient"+clientNumber,clientStub);
                 bindName = "RMIClient"+clientNumber;
