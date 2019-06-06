49b7c43e02c9bed9396ee97804197c23b0ac220f
InnocenteFederico <federico.innocente@mail.polimi.it>
2019-06-06 11:31:41 +0200

    .

--- src/main/java/it/polimi/ingsw/view/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/view/cli/CLI.java
@@ -361,7 +361,7 @@ public class CLI extends RemoteView {
             }
         }
 
-        return new WeaponTargetChoiceEvent(getUser(), targetCharacter);
+        return new WeaponPlayersTargetChoiceEvent(getUser(), targetCharacter);
     }
 
     @Override
