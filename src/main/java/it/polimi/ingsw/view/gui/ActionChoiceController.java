b384e59960a355179af2bde949443e45065b21d8
frank <franci01@hotmail.it>
2019-07-02 17:48:30 +0200

    aggiornamento grafica

---
+++ src/main/java/it/polimi/ingsw/view/gui/ActionChoiceController.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.view.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ActionChoiceController {
+
+    @FXML
+    private Button moveButton;
+
+    @FXML
+    private Button shotButton;
+
+    @FXML
+    private Button grabButton;
+
+}
+
---
+++ src/main/java/it/polimi/ingsw/view/gui/CharacterChoiceController.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.view.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CharacterChoiceController {
+
+    @FXML
+    private Button dozerButton;
+
+    @FXML
+    private Button violetButton;
+
+    @FXML
+    private Button sprogButton;
+
+    @FXML
+    private Button bansheeButton;
+
+    @FXML
+    private Button dstructorButton;
+
+
+}
