ebd400b74ae344ed5b6660c3392598c11456206a
InnocenteFederico <federico.innocente@mail.polimi.it>
2019-07-03 14:14:31 +0200

    Aggiunti i javaDoc del DeathManager e del Controller

--- src/main/java/it/polimi/ingsw/controller/DeathManager.java
+++ src/main/java/it/polimi/ingsw/controller/DeathManager.java
@@ -10,20 +10,28 @@ import it.polimi.ingsw.model.player.Player;
 import it.polimi.ingsw.utils.Decoder;
 import it.polimi.ingsw.utils.Encoder;
 
+/**
+ * Class to manage the death and the respawn of the players
+ *
+ * @author Federico Innocente
+ */
 public class DeathManager {
     private Controller controller;
     private GameModel model;
     private Player deadPlayer;
     private RoundManager roundManager;
 
-    public DeathManager(Controller controller, GameModel model, Player deadPlayer, RoundManager roundManager) {
+    DeathManager(Controller controller, GameModel model, Player deadPlayer, RoundManager roundManager) {
         this.controller = controller;
         this.model = model;
         this.deadPlayer = deadPlayer;
         this.roundManager = roundManager;
     }
 
-    public void manageKill(){
+    /**
+     * Method to manage the death of a player
+     */
+    void manageKill() {
         controller.getGameManager().collectPlayerBoardPoints(deadPlayer);
         updateGameTrack();
         deadPlayer.getPlayerBoard().addSkull();
@@ -32,34 +40,39 @@ public class DeathManager {
     }
 
     /**
-     *
-     * when the player send the square choice, controller call spawn()
+     * Give to the player a new powerUp and send him the respawn request
      */
-    public void respawnPlayer() {
+    void respawnPlayer() {
         deadPlayer.addSpawnPowerUp((PowerUp) model.getGameboard().getPowerUpDeck().draw());
         controller.callView(new RespawnRequestEvent(deadPlayer.getUsername(), Encoder.encodePowerUpsType(deadPlayer.getPowerUps()), Encoder.encodePowerUpColour(deadPlayer.getPowerUps())));
     }
 
-    public void spawn(String powerUp, CubeColour cardColour){
+    /**
+     * Spawn the player in the chosen spawn square
+     *
+     * @param powerUp    is the type of the powerUp chosen for the respawn
+     * @param cardColour is the colour of the powerUp chosen for the respawn and of the chosen spawn square
+     */
+    public void spawn(String powerUp, CubeColour cardColour) {
         PowerUp chosenPowerUp = Decoder.decodePowerUp(deadPlayer, powerUp, cardColour);
-        for (SpawnSquare possibleSpawnSquare: model.getGameboard().getMap().getSpawnSquares()) {
-            if (possibleSpawnSquare.getSquareColour().equals(chosenPowerUp.getColour().toString() ) ){
+        for (SpawnSquare possibleSpawnSquare : model.getGameboard().getMap().getSpawnSquares()) {
+            if (possibleSpawnSquare.getSquareColour().equals(chosenPowerUp.getColour().toString())) {
                 deadPlayer.setPosition(possibleSpawnSquare);
                 deadPlayer.discardPowerUp(chosenPowerUp);
                 model.getGameboard().getPowerUpDeck().discardCard(chosenPowerUp);
             }
         }
-
         if (deadPlayer.isDead()) {
             deadPlayer.invertDeathState();
             roundManager.manageDeadPlayers();
-        }
-        else{
+        } else
             controller.getGameManager().getCurrentRound().nextPhase();
-        }
     }
 
-    private void updateGameTrack(){
+    /**
+     * Update the game track, removing a skull and adding the killer tokens
+     */
+    private void updateGameTrack() {
         GameTrack gameTrack = controller.getGameManager().getModel().getGameboard().getGameTrack();
         if (gameTrack.getSkullBox() > 0) {
             if (deadPlayer.getPlayerBoard().getDamageAmount() == 11)
