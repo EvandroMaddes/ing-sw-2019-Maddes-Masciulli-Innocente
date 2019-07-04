55e97970875215acd09b2ad62fe606c965505c5f
InnocenteFederico <federico.innocente@mail.polimi.it>
2019-07-04 02:29:35 +0200

    aggiunto javadoc di decoder e encoder

--- src/main/java/it/polimi/ingsw/utils/Decoder.java
+++ src/main/java/it/polimi/ingsw/utils/Decoder.java
@@ -11,17 +11,27 @@ import it.polimi.ingsw.model.player.Player;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class to decode the lite version of the object coming from the client to the controller
+ */
 public class Decoder {
 
-    private Decoder(){
-
+    private Decoder() {
     }
 
-    public static ArrayList<PowerUp> decodePowerUpsList(Player powerUpsOwner, String[] powerUpType, CubeColour[] powerUpColour){
+    /**
+     * Decode a list of powerUps type and the associated powerUps to a list of powerUps, knowing their owner
+     *
+     * @param powerUpsOwner is the powerUps owner
+     * @param powerUpType   are the powerUps type
+     * @param powerUpColour are the powerUps colour
+     * @return a list of powerUps of the player
+     */
+    public static ArrayList<PowerUp> decodePowerUpsList(Player powerUpsOwner, String[] powerUpType, CubeColour[] powerUpColour) {
         ArrayList<PowerUp> decodedPowerUps = new ArrayList<>();
-        for(int i = 0; i < powerUpType.length; i++){
-            for (PowerUp p:powerUpsOwner.getPowerUps()){
-                if (p.getName().equals(powerUpType[i]) && p.getColour() == powerUpColour[i]){
+        for (int i = 0; i < powerUpType.length; i++) {
+            for (PowerUp p : powerUpsOwner.getPowerUps()) {
+                if (p.getName().equals(powerUpType[i]) && p.getColour() == powerUpColour[i]) {
                     decodedPowerUps.add(p);
                     powerUpType[i] = "";
                 }
@@ -30,45 +40,89 @@ public class Decoder {
         return decodedPowerUps;
     }
 
-    public static PowerUp decodePowerUp (Player powerUpOwner, String powerUpType, CubeColour powerUpColour){
-        for (PowerUp p: powerUpOwner.getPowerUps()) {
+    /**
+     * Decode a single powerUps, knowing his type, colour and owner
+     *
+     * @param powerUpOwner  is the powerUp owner
+     * @param powerUpType   is the powerUp type
+     * @param powerUpColour is the powerUp colour
+     * @return the reference to the player's powerUp
+     */
+    public static PowerUp decodePowerUp(Player powerUpOwner, String powerUpType, CubeColour powerUpColour) {
+        for (PowerUp p : powerUpOwner.getPowerUps()) {
             if (p.getName().equals(powerUpType) && p.getColour() == powerUpColour)
                 return p;
         }
         throw new UnsupportedOperationException("Something wrong in powerUp decode");
     }
 
-    public static Square decodeSquare (int squareX, int squareY, Map map){
+    /**
+     * Decode a square from his coordinates
+     *
+     * @param squareX is the square row
+     * @param squareY is teh square column
+     * @param map     is the game map
+     * @return the decoded square
+     */
+    public static Square decodeSquare(int squareX, int squareY, Map map) {
         return map.getSquareMatrix()[squareX][squareY];
     }
 
-    public static ArrayList<Object> decodePlayerListAsObject (List<Character> charactersList, List<Player> playerList){
+    /**
+     * Decode a list of character into a list of object with the reference of their player
+     *
+     * @param charactersList is teh list of character to decode
+     * @param playerList     is the list of all the players
+     * @return a list of player
+     */
+    public static ArrayList<Object> decodePlayerListAsObject(List<Character> charactersList, List<Player> playerList) {
         ArrayList<Object> decodedList = new ArrayList<>();
-        for (Player p: playerList){
+        for (Player p : playerList) {
             if (charactersList.contains(p.getCharacter()))
                 decodedList.add(p);
         }
         return decodedList;
     }
 
-    public static Player decodePlayerFromCharacter(Character character, List<Player> playerList){
-        for (Player p:playerList){
+    /**
+     * Decode a player from his character
+     *
+     * @param character  is the player character
+     * @param playerList is a list of all the players in the game
+     * @return the player that use the chosen character
+     */
+    public static Player decodePlayerFromCharacter(Character character, List<Player> playerList) {
+        for (Player p : playerList) {
             if (p.getCharacter() == character)
                 return p;
         }
         throw new UnsupportedOperationException("Something wrong in player decode");
     }
 
-    public static Player decodePlayerFromUsername(String username, List<Player> playerList){
-        for (Player p: playerList){
+    /**
+     * Decode a player from his username
+     *
+     * @param username   is the player username
+     * @param playerList is a list of all the players in teh game
+     * @return the player with the username
+     */
+    public static Player decodePlayerFromUsername(String username, List<Player> playerList) {
+        for (Player p : playerList) {
             if (p.getUsername().equals(username))
                 return p;
         }
         throw new UnsupportedOperationException("Something wrong in player decode");
     }
 
-    public static Weapon decodePlayerWeapon (Player weaponOwner, String weaponName){
-        for (int i = 0; i < weaponOwner.getNumberOfWeapons(); i++){
+    /**
+     * Decode a weapon from it's name knowing its owner
+     *
+     * @param weaponOwner is the weapon owner
+     * @param weaponName  is the weapon name
+     * @return the decoded weapon
+     */
+    public static Weapon decodePlayerWeapon(Player weaponOwner, String weaponName) {
+        for (int i = 0; i < weaponOwner.getNumberOfWeapons(); i++) {
             if (weaponOwner.getWeapons()[i].getName().equals(weaponName))
                 return weaponOwner.getWeapons()[i];
         }
--- src/main/java/it/polimi/ingsw/utils/Encoder.java
+++ src/main/java/it/polimi/ingsw/utils/Encoder.java
@@ -11,49 +11,93 @@ import it.polimi.ingsw.model.player.Player;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class to encode in a lite version the object of the model to send them to the client threw the network
+ *
+ * @author Federico Innocente
+ */
 public class Encoder {
 
-    public static ArrayList<Character> encodePlayerTargets(List<Player> targets){
+    private Encoder() {
+    }
+
+    /**
+     * Encode a list of player in a list of their character
+     *
+     * @param targets is the list of the players to encode
+     * @return a list of all the players character
+     */
+    public static ArrayList<Character> encodePlayerTargets(List<Player> targets) {
         ArrayList<Character> targetsLite = new ArrayList<>();
-        for (Player p: targets) {
+        for (Player p : targets) {
             targetsLite.add(p.getCharacter());
         }
         return targetsLite;
     }
 
-    public static int[] encodeSquareTargetsX(List<Square> targets){
+    /**
+     * Encode a list of square into an array of their row
+     *
+     * @param targets is a list of squares
+     * @return an array of all the squares row
+     */
+    public static int[] encodeSquareTargetsX(List<Square> targets) {
         int[] targetsX = new int[targets.size()];
-        for(int i = 0 ; i < targets.size(); i++){
+        for (int i = 0; i < targets.size(); i++) {
             targetsX[i] = targets.get(i).getRow();
         }
         return targetsX;
     }
 
-    public static int[] encodeSquareTargetsY(List<Square> targets){
+    /**
+     * Encode a list of square into an array of their column
+     *
+     * @param targets is a list of squares
+     * @return an array of all the squares column
+     */
+    public static int[] encodeSquareTargetsY(List<Square> targets) {
         int[] targetsY = new int[targets.size()];
-        for(int i = 0 ; i < targets.size(); i++){
+        for (int i = 0; i < targets.size(); i++) {
             targetsY[i] = targets.get(i).getColumn();
         }
         return targetsY;
     }
 
+    /**
+     * Encode a list of weapons in a list of their names (String)
+     *
+     * @param weapons is the list of weapon to decode
+     * @return a list of all the weapons name
+     */
     public static ArrayList<String> encodeWeaponsList(List<Weapon> weapons) {
         ArrayList<String> weaponsLite = new ArrayList<>();
-        for (Weapon w:weapons){
+        for (Weapon w : weapons) {
             weaponsLite.add(w.getName());
         }
         return weaponsLite;
     }
 
-    public static String[] encodeWeaponsIntoArray(List<Weapon> weapons){
+    /**
+     * Encode a list of weapons in aa array of their names (String)
+     *
+     * @param weapons is the list of weapon to decode
+     * @return an array of all the weapons name
+     */
+    public static String[] encodeWeaponsIntoArray(List<Weapon> weapons) {
         String[] weaponsLite = new String[weapons.size()];
-        for (int i = 0; i < weapons.size(); i++){
+        for (int i = 0; i < weapons.size(); i++) {
             weaponsLite[i] = weapons.get(i).getName();
         }
         return weaponsLite;
     }
 
-    public static String[] encodePowerUpsType (List<PowerUp> powerUps){
+    /**
+     * Encode a list of powerUps in an array of their type (String)
+     *
+     * @param powerUps il the list of powerUps to encode
+     * @return is teh array of encoded powerUps type
+     */
+    public static String[] encodePowerUpsType(List<PowerUp> powerUps) {
         String[] powerUpsType = new String[powerUps.size()];
         for (int i = 0; i < powerUps.size(); i++) {
             powerUpsType[i] = powerUps.get(i).getName();
@@ -61,7 +105,13 @@ public class Encoder {
         return powerUpsType;
     }
 
-    public static CubeColour[] encodePowerUpColour (List<PowerUp> powerUps){
+    /**
+     * Encode a list of powerUps in an array of their colour (CubeColour)
+     *
+     * @param powerUps il the list of powerUps to encode
+     * @return is teh array of encoded powerUps colour
+     */
+    public static CubeColour[] encodePowerUpColour(List<PowerUp> powerUps) {
         CubeColour[] powerUpsColour = new CubeColour[powerUps.size()];
         for (int i = 0; i < powerUps.size(); i++) {
             powerUpsColour[i] = powerUps.get(i).getColour();
@@ -69,17 +119,30 @@ public class Encoder {
         return powerUpsColour;
     }
 
-    public static Character[] encodeDamageTokenList(List<DamageToken> damageTokensList){
+    /**
+     * Encode a damage token list into an array of the respective character
+     *
+     * @param damageTokensList is the list of damage tokens
+     * @return the array of tokens character
+     */
+    public static Character[] encodeDamageTokenList(List<DamageToken> damageTokensList) {
         Character[] marksLite = new Character[damageTokensList.size()];
-        for (int i = 0; i < damageTokensList.size(); i++){
+        for (int i = 0; i < damageTokensList.size(); i++) {
             marksLite[i] = damageTokensList.get(i).getPlayer().getCharacter();
         }
         return marksLite;
     }
 
-    public static Character[] encodeDamagesTokenArray(DamageToken[] damages, int numberOfDamages){
+    /**
+     * Encode a damage token array into an array of the respective character
+     *
+     * @param damages         is the array of damage tokens
+     * @param numberOfDamages is the number of damage tocken into the array
+     * @return the array of tokens character
+     */
+    public static Character[] encodeDamagesTokenArray(DamageToken[] damages, int numberOfDamages) {
         Character[] damagesLite = new Character[numberOfDamages];
-        for (int i = 0; i < numberOfDamages; i++){
+        for (int i = 0; i < numberOfDamages; i++) {
             damagesLite[i] = damages[i].getPlayer().getCharacter();
         }
         return damagesLite;
