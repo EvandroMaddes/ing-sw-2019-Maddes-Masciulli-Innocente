c608ccacbbace5bbc71bf315ae3d6cc9c357913c
InnocenteFederico <federico.innocente@mail.polimi.it>
2019-07-04 11:53:00 +0200

    Aggiunto javadoc virtual view

--- src/main/java/it/polimi/ingsw/view/VirtualView.java
+++ src/main/java/it/polimi/ingsw/view/VirtualView.java
@@ -6,43 +6,59 @@ import it.polimi.ingsw.event.controllerviewevent.ControllerViewEvent;
 import java.util.*;
 
 /**
- * todo O eventi girati al controller oppure girati alla remote view
- *
- * E' observer del model e observable del controller:
- * 1) Inoltra gli eventi ricevuti dal model attraverso la rete ( alla view del client);
- *
- * 2) Riceve eventi dalla view del client attraverso la rete e li inoltra al controller.
- *
+ * Is a model observer and controller observable
+ * Has three functions :
+ * 1 - Send the messages receved from the model to controller threw the networck
+ * 2 - Send the message from the client to the controller
+ * 3 - Take the request messages from the controller to the clients
  */
 
-public class VirtualView extends Observable implements Observer{
+public class VirtualView extends Observable implements Observer {
 
-
+    /**
+     * Is the user of who is associated to the virtual view
+     */
     private String user;
+    /**
+     * Is the message from the client that has to be send to the controller
+     */
     private Event toController;
+    /**
+     * Is the message from the controller that has to be send to the client
+     */
     private Event toRemoteView;
+    /**
+     * Is the queue of update messages from the model that has to be send to the clients
+     */
     private Queue<Event> modelUpdateQueue;
+    /**
+     * Flag to denote if the player associated to the virtual view is connect
+     */
     private boolean playerConnected;
 
     /**
      * Every player has a his own view
+     *
      * @param user
      */
-    public VirtualView(String user)
-    {
+    public VirtualView(String user) {
         this.user = user;
         playerConnected = true;
         modelUpdateQueue = new LinkedList<>();
     }
 
     /**
-     *
-     * @return
+     * Getter method
+     * @return the message that has to go to the client
      */
     public Event getToRemoteView() {
         return toRemoteView;
     }
 
+    /**
+     * Getter method
+     * @return the update messages queue
+     */
     public Queue<Event> getModelUpdateQueue() {
         return modelUpdateQueue;
     }
@@ -52,7 +68,7 @@ public class VirtualView extends Observable implements Observer{
      * this means that message should be send to controller.
      * Remember: VIRTUAL_VIEW IS AN OBSERVABLE FROM THE CONTROLLER
      */
-    public void toController(Event message){
+    public void toController(Event message) {
         setToController(message);
         setChanged();
         notifyObservers(this.getToController());
@@ -62,43 +78,53 @@ public class VirtualView extends Observable implements Observer{
      * this method is called from the model through notifyObservers() defined in the model.
      * this method set event that should be send to RemoteView.
      * Remember: VIRTUAL_VIEW IS AN OBSERVER OF THE MODEL
-     * @param o
+     *
+     * @param o Is the observable
      * @param arg message
      */
     @Override
     public void update(Observable o, Object arg) {
-        if(((Event)arg).getUser().equals("BROADCAST")||((Event) arg).getUser().equals(user)) {
+        if (((Event) arg).getUser().equals("BROADCAST") || ((Event) arg).getUser().equals(user)) {
             modelUpdateQueue.offer((Event) arg);
         }
     }
 
 
-
     /**
-     *
-     * @param toController
+     * Set the message from the server to the controller
+     * @param toController is the message
      */
-    public void setToController(Event toController) {
+    private void setToController(Event toController) {
         this.toController = toController;
     }
 
     /**
-     *
-     * @return
+     * Getter method
+     * @return the message to the controller
      */
-    public Event getToController() {
+    private Event getToController() {
         return toController;
     }
-    
-    public void callRemoteView(ControllerViewEvent message){
+
+    /**
+     * Method called by the controller to set the request messages to send to the client
+     * @param message is the message for the client
+     */
+    public void callRemoteView(ControllerViewEvent message) {
         toRemoteView = message;
     }
 
-    public void setPlayerConnected(){
+    /**
+     * Set the player connected
+     */
+    public void setPlayerConnected() {
         playerConnected = true;
     }
 
-    public void setPlayerDisonnected(){
+    /**
+     * Set the player disconnected
+     */
+    public void setPlayerDisonnected() {
         playerConnected = false;
     }
 }
